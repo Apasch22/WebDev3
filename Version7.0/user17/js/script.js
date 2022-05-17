d12d1a52e4874a88a284cbca899b8c6a1b1d7be5
IsaiahSloan <103592510+IsaiahSloan@users.noreply.github.com>
2022-05-16 21:41:22 -0500

    added in all my files
    i use a different editor because i like it more than vs code, so i just had to copy paste the files from my other repository

--- Version7.0/user17/js/script.js
+++ Version7.0/user17/js/script.js
@@ -1,1 +1,13 @@
-// hello
+// hide and show past devlogs in devlog.html
+function toggleDiv() {
+  var x = document.getElementById("previous");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+//display current year at the bottom of the page
+const p = new Date();
+document.getElementById("getYear").innerHTML = p.getFullYear();
